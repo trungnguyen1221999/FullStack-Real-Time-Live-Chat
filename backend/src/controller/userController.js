aef03cf9c78247b722fc1cd570876913306b4aab
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-11-05 12:24:59 +0200

    create vite for front end

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,3 +1,10 @@
 export const authMe = async (req, res) => {
-    res.status(200).json({ message: 'Authenticated user', success: true, error: false, user: req.user });
+    try {
+        const user = req.user;
+        return res.status(200).json({ message: 'User authenticated successfully', success: true, error: false, user }); 
+    }
+    catch (error) {
+        console.error('Error in authMe controller:', error);
+        return res.status(500).json({ message: 'Internal server error', success: false, error: true });
+    }
 }
