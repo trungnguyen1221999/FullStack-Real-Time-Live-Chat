229e31601691682371992af637787fb0aaf23fac
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-11-06 09:26:57 +0200

    add light mode and dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,28 @@ import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
 import { Toaster } from 'sonner';
 import ProtectedRoute from './components/ProtectedRoute';
+import { ThemeProvider } from './components/ThemeProvider';
+import { ModeToggle } from './components/ModeToggle';
 function App() {
  
   return (
     <>
-    <Toaster richColors />
-      <div className='min-h-screen bg-gradient-purple'>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/signin" element={<SignInPage />} />
-            {/* protected routes */}
-          <Route element={<ProtectedRoute />}>  <Route path="/" element={<ChatPage />} /></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Toaster richColors />
+        <div className='min-h-screen bg-gradient-purple'>
+          <div className='absolute top-4 right-4'>
+             <ModeToggle />
+          </div>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              {/* protected routes */}
+            <Route element={<ProtectedRoute />}>  <Route path="/" element={<ChatPage />} /></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+   </ThemeProvider>
     </>
   )
 }
